38ee0741797413059df71d6124ce957db0a3e663
Rishu <ritesh324@icloud.com>
2018-02-26 13:28:10 -0700

    updated server.js

--- server.js
+++ server.js
@@ -1,33 +1,85 @@
 var express = require('express');
 var http = require('http');
 var bodyParser = require('body-parser');
+var passport = require('passport');
+var authController = require('./auth');
+db = require('./db')(); //global hack
 
 var app = express();
-app.use(bodyParser.text({
-    type: function(req) {
-    return  'text';
-}
-}));
+//app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(passport.initialize());
+
+var router = express.Router();
 
 app.post('/post', function (req, res) {
-    console.log(req.body);
-    res = res.status(200);
-    if (req.get('Content-Type')) {
-        console.log("Content-Type: " + req.get('Content-Type'));
+            console.log(req.body);
+            res = res.status(200);
+            if (req.get('Content-Type')) {
+                console.log("Content-Type: " + req.get('Content-Type'));
+                res = res.type(req.get('Content-Type'));
+            }
+            res.send(req.body);
+        }
+    );
+
+router.put('/', function (req, res) {
+  console.log(req.body);
+  res = res.status(200);
+  if (req.get('Content-Type')) {
+      console.log("Content-Type: " + req.get('Content-Type'));
+      res = res.type(req.get('Content-Type'));
+  }
+  res.send(req.body);
+});
+
+router.get('/', function(req, res) {
+  console.log(req.body);
+  res = res.status(200);
+  if (req.get('Content-Type')) {
+      console.log("Content-Type: " + req.get('Content-Type'));
+      res = res.type(req.get('Content-Type'));
+      res.send(req.body);
+  }
+  else {
+    res.send("no content sent");
+  }
+})
+
+router.post('/signup', function(req, res) {
+    if (!req.body.username || !req.body.password) {
+        res.json({success: false, msg: 'Please pass username and password.'});
+    } else {
+        var newUser = {
+            username: req.body.username,
+            password: req.body.password
+        };
+        // save the user
+        db.save(newUser); //no duplicate checking
+        res.json({success: true, msg: 'Successful created new user.'});
     }
-    res.send(req.body);
 });
 
-app.get('/get', function (req, res) {
+router.post('/signin', function(req, res) {
 
+        var user = db.findOne(req.body.username);
+
+        if (!user) {
+            res.status(401).send({success: false, msg: 'Authentication failed. User not found.'});
+        }
+        else {
+            // check if password matches
+            if (req.body.password == user.password)  {
+                var userToken = { id : user.id, username: user.username };
+                var token = jwt.sign(userToken, process.env.SECRET_KEY);
+                res.json({success: true, token: 'JWT ' + token});
+            }
+            else {
+                res.status(401).send({success: false, msg: 'Authentication failed. Wrong password.'});
+            }
+        };
 });
 
-app.put('/put', function (req, res) {
-
-});
-
-app.delete('/delete', function (req, res) {
-
-});
-
-http.createServer(app).listen(process.env.PORT || 8080);
+app.use('/', router);
+app.listen(process.env.PORT || 8080);
